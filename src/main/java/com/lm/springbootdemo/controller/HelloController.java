94cb0fc51a6c7d388ff1222e1fe9c5833a0f0383
moxiaoxiao123456 <lm123moxiaoxiao@163.com>
2020-05-21 14:33:11 +0800

    first commit

---
+++ src/main/java/com/lm/springbootdemo/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.lm.springbootdemo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liming
+ * <p>
+ * created at 2020/5/21 13:49
+ */
+@RestController
+@RequestMapping("/hello")
+@Slf4j
+public class HelloController {
+
+    @RequestMapping(value="/jenkins",method = RequestMethod.GET)
+    public String helloJenkins(){
+
+        return "hello jenkins!!!!";
+    }
+}
